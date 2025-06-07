beaf8f708ecbb216f2c3e86828ceeca98e7b3220
ramani hasti <ramanihasti053@gmail.com>
2025-06-07 21:25:43 +0530

    Github Tutorial and create branch Ways

---
+++ Github-vscode.js
@@ -0,0 +1,29 @@
+// Vs code se Github pe code ko kese chadhate hain?
+
+// → first aapke computer/system mein Gitub sign up kar lena hain.
+// → agar already hain to sign in karna hain.
+// → unke bad aapko ek repositiry create karni hain.
+// → aapke pass repository create kari hui nahi to first create karni hain.
+// → unka code GitWay.js name ki file me hain.
+// → agar aapke pass alredy create hain to code ko github pe push karna hain.
+// → code ko github pe push karne ka command niche hain.
+// → …or push an existing repository from the command line:
+// → git remote add origin https://github.com/ramanihasti/git-demo.git
+// → ye vala url deta zaruri hain.
+// → inse pata chalta hain ki konse url par code ko bhajna hain.
+// → 2. git branch -M main
+// → agar aap main branch pe nahi ho to main branch pe aana hain.
+// → 3. git push -u origin main
+// → code ko github pe push karne ka command.
+
+// ak hi bar push karne ke liye [git push -u origin main] ye command likhna hain.
+// unke bad code ko push karne ke liye only git push likhoge to bhi chalega.
+
+// abhi jinko ko bhi chahiye unko de sakte ho.
+// isi code pe multiple person work bhi kar sakte hain.
+// aap unkko ye url bhej donge: https://github.com/ramanihasti/git-demo.git
+// abhi kisi bhi person ko code pe work karna chahta hain to ye command apne PC me cmd par likhega.
+// git clone https://github.com/ramanihasti/git-demo.git
+// fir sabhi code unke PC me bhi aa jayega.
+// dusre person me unke PC me code ko add kiya hain wo aapke PC me lane ke liye: git pull
+// ye command likhoge to aapke system me bhi code aa jayega.
